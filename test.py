99a47b00e97ad39d400b53d0eaebaad6b5826846
Dhrubo Saha <dhrubo@amazon.com>
2023-03-15 15:17:53 -0700

    testing
    Signed-off-by: Dhrubo Saha <dhrubo@amazon.com>

---
+++ test.py
@@ -0,0 +1,18 @@
+import torch
+from torch import nn
+
+
+class IValueProcessing(nn.Module):
+    def __init__(self):
+        super(IValueProcessing, self).__init__()
+
+    def forward(self, testInput: torch.Tensor):
+        return torch.sum(testInput).item()
+
+
+jit_processing = torch.jit.script(IValueProcessing())
+
+print(jit_processing.forward(torch.Tensor([1, 2, 3])))
+
+print(jit_processing.code)
+torch.jit.save(jit_processing, "ivalue_jit.pt")
